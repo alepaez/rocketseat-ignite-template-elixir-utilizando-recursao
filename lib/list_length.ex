e7fd5b07a8765f039065bf844e47ec2a9111877a
Alexandre Paez Sequeira <alepaezseq@gmail.com>
2021-09-14 23:44:00 -0300

    done

--- lib/list_length.ex
+++ lib/list_length.ex
@@ -1,5 +1,11 @@
 defmodule ListLength do
-  def call() do
-    #TO DO
+  def call(list \\ [], count \\ 0)
+
+  def call([_ | tail], count) do
+    call(tail, count + 1)
+  end
+
+  def call([], count) do
+    count
   end
 end
